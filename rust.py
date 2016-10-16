61689eee8ef81955d0599dcf7925019b2be0a287
Simon Bastien-Filiatrault (Gopoi) <root@gopoi.net>
2016-10-16 13:39:12 -0400

    Initial commit.

---
+++ rust.py
@@ -0,0 +1,22 @@
+class Item(object):
+    def __init__(self, name, **item_list):
+        self.name = name
+        self.item_list = item_list
+    
+    def get_items(self):
+        return self.item_list
+
+class ItemCollection(object):
+    def __init__(self, name, collection=None):
+        self.name = name
+        if not collection:
+            self.collection == {}
+        else:
+            self.collection = collection
+
+    def add_item(self, item):
+        self.collection[item.name] = item
+
+    def get_materials(self, item_name, multiplier=1):
+        if not self.collection[item_name]:
+            
